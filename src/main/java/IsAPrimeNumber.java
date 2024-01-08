e37ab52ece5ab16eb2bb1bbe4320bb0765cd39cd
d33t-ya <140362637+d33t-ya@users.noreply.github.com>
2024-01-08 12:31:44 -0800

    YAY! The code passed my test!

--- src/main/java/IsAPrimeNumber.java
+++ src/main/java/IsAPrimeNumber.java
@@ -2,7 +2,19 @@ public class IsAPrimeNumber {
     /*
         This function returns true when the given number is a prime number
      */
-    public static boolean isAPrimeNumber(long num) {  // DO NOT change the function name, return type, and parameter(s).
-        return false; // TODO: placeholder. To be replace with real code.
+    public static boolean isAPrimeNumber(long num) {
+        // DO NOT change the function name, return type, and parameter(s).
+        /*
+         * Prime num can only be divided by 1 and the num itself
+         */
+        for (int i = 2; i < num ; i++) {
+            if(num%i == 0){
+                return false;
+            }
+
+        }
+
+
+        return true; // TODO: placeholder. To be replace with real code.
     }
 }
--- src/test/java/PrimeNumberTest.java
+++ src/test/java/PrimeNumberTest.java
@@ -1,8 +1,26 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+
 public class PrimeNumberTest {
     @Test
     public void myAwesomeTest() {
         // TODO: add your test
+
+        //inputs: number
+        int input = 2;
+
+        //expected output:
+        boolean expect = true;
+
+        //actual output:
+        boolean actual = IsAPrimeNumber.isAPrimeNumber(input);
+
+        //is expected the same as usual
+        assertEquals(expect, actual);
+
+
+
     }
 }
